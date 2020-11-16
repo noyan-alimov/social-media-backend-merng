85db465908ac27237db1d0b127c6766cf9570f06
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-16 17:39:59 +0000

    implemented register user logic

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,13 +1,29 @@
 const gql = require('graphql-tag');
 
 module.exports = gql`
-	type Query {
-		getPosts: [Post]
-	}
 	type Post {
 		id: ID!
 		body: String!
 		createdAt: String!
 		username: String!
+	}
+	type User {
+		id: ID!
+		email: String!
+		token: String!
+		username: String!
+		createdAt: String!
+	}
+	input RegisterInput {
+		username: String!
+		password: String!
+		confirmPassword: String!
+		email: String!
+	}
+	type Query {
+		getPosts: [Post]
+	}
+	type Mutation {
+		register(registerInput: RegisterInput): User!
 	}
 `;
