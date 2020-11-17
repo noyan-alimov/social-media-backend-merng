f45983ee9d4e8fb7c421da86ab7443fd8cbde563
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-17 11:14:22 +0000

    implemented liking posts feature and counting likes and comments

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth');
@@ -30,6 +30,10 @@ module.exports = {
 		async createPost(_, { body }, context) {
 			const user = checkAuth(context);
 
+			if (body.trim() === '') {
+				throw new Error('Post body must not be empty');
+			}
+
 			const newPost = new Post({
 				body,
 				user: user.id,
@@ -38,6 +42,10 @@ module.exports = {
 			});
 
 			const post = await newPost.save();
+
+			context.pubsub.publish('NEW_POST', {
+				newPost: post,
+			});
 
 			return post;
 		},
@@ -55,6 +63,32 @@ module.exports = {
 			} catch (err) {
 				throw new Error(err);
 			}
+		},
+		async likePost(_, { postId }, context) {
+			const { username } = checkAuth(context);
+
+			const post = await Post.findById(postId);
+			if (post) {
+				if (post.likes.find(like => like.username === username)) {
+					// Post already liked, unlike it
+					post.likes = post.likes.filter(like => like.username !== username);
+				} else {
+					// Post not liked yet, like it
+					post.likes.push({
+						username,
+						createdAt: new Date().toISOString(),
+					});
+				}
+				await post.save();
+				return post;
+			} else {
+				throw new UserInputError('Post not found');
+			}
+		},
+	},
+	Subscription: {
+		newPost: {
+			subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST'),
 		},
 	},
 };
