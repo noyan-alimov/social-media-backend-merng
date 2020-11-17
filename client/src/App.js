ac6c6cc145579fbbc5d1a76a20d1a5200b3264cf
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-17 13:39:57 +0000

    setup semantic ui and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import { Container } from 'semantic-ui-react';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Container>
+				<MenuBar />
+				<Route exact path='/' component={Home} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+			</Container>
+		</BrowserRouter>
+	);
 }
 
 export default App;
