6362bf280e70c8b940f4a13ba1dcff3ffa6ced7b
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-17 15:28:16 +0000

    implemented displaying posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard';
 
 function Home() {
-	return <div>Home</div>;
+	const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+	return (
+		<Grid columns={3} divided>
+			<Grid.Row>
+				<h1>Recent posts</h1>
+			</Grid.Row>
+			<Grid.Row>
+				{loading ? (
+					<h1>Loading posts...</h1>
+				) : (
+					data.getPosts &&
+					data.getPosts.map(post => (
+						<Grid.Column key={post.id} style={{ marginBottom: '1.2rem' }}>
+							<PostCard post={post} />
+						</Grid.Column>
+					))
+				)}
+			</Grid.Row>
+		</Grid>
+	);
 }
+
+const FETCH_POSTS_QUERY = gql`
+	{
+		getPosts {
+			id
+			body
+			createdAt
+			username
+			likeCount
+			commentCount
+			likes {
+				username
+			}
+			comments {
+				id
+				username
+				body
+				createdAt
+			}
+		}
+	}
+`;
 
 export default Home;
